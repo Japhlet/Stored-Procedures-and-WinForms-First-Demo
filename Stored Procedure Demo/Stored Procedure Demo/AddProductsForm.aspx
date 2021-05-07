1d3b8b7d5868eeefc57bf3fd0cb30d3419170ace
Japhlet Etyang <jetyang@fintech-group.com>
2021-05-07 11:55:05 +0300

    first commit

---
+++ Stored Procedure Demo/Stored Procedure Demo/AddProductsForm.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddProductsForm.aspx.cs" Inherits="Stored_Procedure_Demo.AddProductsForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="padding: 5px">
+            <h2>Add Data using SQL Stored Procedure</h2>
+
+            <table class="table table-condensed" style="width: 500px">
+                <tr>
+                    <td>Product Name:</td>
+                    <td>
+                        <asp:TextBox ID="txtProductName" runat="server" placeholder="Enter Product Name" width="250px"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Product Description:</td>
+                    <td>
+                        <asp:TextBox ID="txtProductDescription" runat="server" placeholder="Enter Product Description" width="250px" TextMode="MultiLine"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2" align="right">
+                        <asp:Button ID="BtnSubmit" runat="server" Text="Save" OnClick="BtnSubmit_Click" />
+                    </td>
+                </tr>
+            </table>
+
+            <asp:Label runat="server" ID="InfoLabel"></asp:Label>
+            <br /><br />
+
+            <asp:GridView runat="server" ID="ProductDataGrid" CssClass="table table-condensed" Width="500px"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Stored Procedure Demo/Stored Procedure Demo/AddProductsForm.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Stored_Procedure_Demo
+{
+    public partial class AddProductsForm : System.Web.UI.Page
+    {
+        public static string connectionString = @"Data Source=DITLPT007\SQL2019; Initial Catalog=StoredProceduresTest; Persist Security Info=True;Integrated Security=true;";
+        SqlConnection con;
+        SqlCommand cmd;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                FillGrid();
+            }
+
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txtProductName == null || txtProductDescription == null || txtProductName.Text.ToString().Trim().Equals("") || 
+                    txtProductDescription.Text.ToString().Trim().Equals("")) 
+                {
+                    InfoLabel.Text = "Please fill all the fields";
+                } else
+                {
+                    con = new SqlConnection(connectionString);
+                    cmd = new SqlCommand();
+                    cmd.Connection = con;
+
+                    con.Open();
+
+                    cmd.CommandText = "sp_AddProducts";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ProductName", txtProductName.Text.ToString());
+                    cmd.Parameters.AddWithValue("@ProductDescription", txtProductDescription.Text.ToString());
+
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(
+                    new SqlCommand("SELECT ProductName, ProductDescription, OnDate FROM Product", con));
+
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    ProductDataGrid.DataSource = dt;
+                    ProductDataGrid.DataBind();
+
+                    adapter.Dispose();
+                    con.Close();
+
+                    InfoLabel.Text = "Product added successfully";
+
+                    txtProductName.Text = "";
+                    txtProductDescription.Text = ""; 
+                }
+
+            } catch (Exception ex)
+            {
+                InfoLabel.Text = "Error: " + ex.Message.ToString();
+            }
+
+        }
+
+        public void FillGrid()
+        {
+            try
+            {
+                con = new SqlConnection(connectionString);
+                con.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(
+                    new SqlCommand("SELECT ProductName, ProductDescription, OnDate FROM Product", con)
+                );
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                ProductDataGrid.DataSource = dt;
+                ProductDataGrid.DataBind();
+
+                adapter.Dispose();
+                con.Close();
+            } catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
